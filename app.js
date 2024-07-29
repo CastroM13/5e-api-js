a52e4b074daaf30ae408f03b9060cfdac7229e42
Matheus Castro <matheus.castro@ciag.org.br>
2024-07-29 01:53:38 -0300

    feat: cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 require('dotenv').config();
+
+app.use(cors());
 
 mongoose.connect(process.env.DBSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
 
