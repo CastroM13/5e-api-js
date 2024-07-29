fa2585a4ac87479e135ad102628236b06a906a26
Matheus Castro <matheus.castro@ciag.org.br>
2024-07-29 01:25:03 -0300

    feat: dotenv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
 
 mongoose.connect(process.env.DBSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
 
