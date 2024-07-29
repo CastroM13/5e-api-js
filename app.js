b3aa74364d9bdc0f9c7c11f2735a3b26ccfd5c7c
Matheus Castro <matheus.castro@ciag.org.br>
2024-07-29 01:21:38 -0300

    feat: init

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect(process.env.DBSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+    console.log('Connected to MongoDB')
+});
+
+const collections = ["ability-scores","alignments","backgrounds","classes","collections","conditions","damage-types","equipment","equipment-categories","feats","features","languages","levels","magic-items","magic-schools","monsters","proficiencies","races","rule-sections","rules","skills","spells","subclasses","subraces","traits","weapon-properties"];
+
+app.get('/query/:collectionName', async (req, res) => {
+    const collectionName = req.params.collectionName;
+    const collection = db.collection(collectionName);
+    try {
+        if (!collections.includes(collectionName)) {
+            return res.status(404).send({error:"Invalid collection"})
+        }
+        const data = await collection.find().toArray();
+        return res.json(data);
+    } catch (err) {
+        return res.status(500).send(err.toString());
+    }
+});
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
